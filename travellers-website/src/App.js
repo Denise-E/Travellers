cbe00a0449fcec2077928af9b50a02866a4d496e
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-05-25 10:37:46 -0300

    Creating new Routes

--- travellers-website/src/App.js
+++ travellers-website/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import Navbar from './components/Navbar.js';
 import Home from './components/pages/Home.js';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
 import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
 
 
@@ -11,6 +15,9 @@ function App() {
        <Navbar/>
         <Routes>
           <Route path='/' exact element={<Home/>} />
+          <Route path='/services' element={<Services/>} />
+          <Route path='/products' element={<Products/>} />
+          <Route path='/sign-up' element={<SignUp/>} />
         </Routes>
      </Router>
      
