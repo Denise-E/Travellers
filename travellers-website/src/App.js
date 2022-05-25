dee3a28a218fed000128bb15325b1300b5784fed
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-05-25 10:07:10 -0300

    Creating first component of the Home

--- travellers-website/src/App.js
+++ travellers-website/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar.js';
+import Home from './components/pages/Home.js';
 import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
 
 
@@ -9,7 +10,7 @@ function App() {
      <Router>
        <Navbar/>
         <Routes>
-          <Route path="/" exact />
+          <Route path='/' exact element={<Home/>} />
         </Routes>
      </Router>
      
