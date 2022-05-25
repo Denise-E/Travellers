820ba8af97ea0459959b45d3e5fd6f4800bf6a8c
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-05-25 10:30:31 -0300

    Creating more Home components

--- travellers-website/src/components/pages/Home.js
+++ travellers-website/src/components/pages/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection.js';
 import '../HeroSection.css';
+import Cards from '../Cards.js';
 
 function Home() {
     return(
         <>
             <HeroSection/>
+            <Cards />
         </>
     )
 }
