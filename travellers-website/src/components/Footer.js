2e0c6901b7704cd07fe079a01cca4b524aefdc8a
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-05-25 11:36:24 -0300

    Adjusting footer

--- travellers-website/src/components/Footer.js
+++ travellers-website/src/components/Footer.js
@@ -1,63 +1,66 @@
 import React from 'react';
-import { Button } from './Button'
 import './Footer.css';
+import { Button } from './Button';
 import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <div className="footer-container">
-        <section className="footer-subscription">
-            <p className="footer-susbcription-heading">
-                Join the Adventure newsletter tot receive our best vacation deals.
-            </p>
-            <p className="footer-susbcription-text">
-                You can unsuscribe at any time
-            </p>
-            <div classname='input-areas'>
-                <form>
-                    <input type="email" name='email' placeholder="Your Email" className="footer-input"/>
-                    <Button buttonStyle='btn--outline'>
-                        Suscribe
-                    </Button>
-                </form>
-            </div>
-        </section>
-        <div className="footer-links">
-            <div className="footer-links-wrapper">
-            <div class='footer-link-items'>
-                <h2>About Us</h2>
-                <Link to='/sign-up'>How it works</Link>
-                <Link to='/'>Testimonials</Link>
-                <Link to='/'>Careers</Link>
-                <Link to='/'>Investors</Link>
-                <Link to='/'>Terms of Service</Link>
-            </div>
-                <div class='footer-link-items'>
-                    <h2>Contact Us</h2>
-                    <Link to='/'>Contact</Link>
-                    <Link to='/'>Support</Link>
-                    <Link to='/'>Destinations</Link>
-                <Link to='/'>Sponsorships</Link>
-            </div>
-            </div>
-            <div className='footer-link-wrapper'>
-            <div class='footer-link-items'>
-                <h2>Videos</h2>
-                <Link to='/'>Submit Video</Link>
-                <Link to='/'>Ambassadors</Link>
-                <Link to='/'>Agency</Link>
-                <Link to='/'>Influencer</Link>
-            </div>
-            <div class='footer-link-items'>
-                <h2>Social Media</h2>
-                <Link to='/'>Instagram</Link>
-                <Link to='/'>Facebook</Link>
-                <Link to='/'>Youtube</Link>
-                <Link to='/'>Twitter</Link>
-            </div>
-            </div>
-        </div>`
-        <section class='social-media'>
+    <div className='footer-container'>
+      <section className='footer-subscription'>
+        <p className='footer-subscription-heading'>
+          Join the Adventure newsletter to receive our best vacation deals
+        </p>
+        <p className='footer-subscription-text'>
+          You can unsubscribe at any time.
+        </p>
+        <div className='input-areas'>
+          <form>
+            <input
+              className='footer-input'
+              name='email'
+              type='email'
+              placeholder='Your Email'
+            />
+            <Button buttonStyle='btn--outline'>Subscribe</Button>
+          </form>
+        </div>
+      </section>
+      <div class='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>About Us</h2>
+            <Link to='/sign-up'>How it works</Link>
+            <Link to='/'>Testimonials</Link>
+            <Link to='/'>Careers</Link>
+            <Link to='/'>Investors</Link>
+            <Link to='/'>Terms of Service</Link>
+          </div>
+          <div class='footer-link-items'>
+            <h2>Contact Us</h2>
+            <Link to='/'>Contact</Link>
+            <Link to='/'>Support</Link>
+            <Link to='/'>Destinations</Link>
+            <Link to='/'>Sponsorships</Link>
+          </div>
+        </div>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>Videos</h2>
+            <Link to='/'>Submit Video</Link>
+            <Link to='/'>Ambassadors</Link>
+            <Link to='/'>Agency</Link>
+            <Link to='/'>Influencer</Link>
+          </div>
+          <div class='footer-link-items'>
+            <h2>Social Media</h2>
+            <Link to='/'>Instagram</Link>
+            <Link to='/'>Facebook</Link>
+            <Link to='/'>Youtube</Link>
+            <Link to='/'>Twitter</Link>
+          </div>
+        </div>
+      </div>
+      <section class='social-media'>
         <div class='social-media-wrap'>
           <div class='footer-logo'>
             <Link to='/' className='social-logo'>
@@ -100,7 +103,7 @@ function Footer() {
               <i class='fab fa-twitter' />
             </Link>
             <Link
-              class='social-icon-link linkedin'
+              class='social-icon-link twitter'
               to='/'
               target='_blank'
               aria-label='LinkedIn'
@@ -110,8 +113,8 @@ function Footer() {
           </div>
         </div>
       </section>
-        </div>
-  )
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
